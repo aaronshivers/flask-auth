207383fcfd2642875b60967752c386c5ab221b4b
aaron shivers <aaorn@moonmail.org>
2019-06-23 14:45:59 -0500

    got logout working

--- project/auth.py
+++ project/auth.py
@@ -1,6 +1,6 @@
 # Import Dependencies
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -49,5 +49,9 @@ def signup():
     return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-  return 'Logout'
+  logout_user()
+  return redirect(url_for('main.index'))
+
+  

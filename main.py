448aa90481701a4596f22b3a7261634da2c18851
aaron shivers <aaorn@moonmail.org>
2019-06-23 11:14:45 -0500

    setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import Dependencies
+from flask import Blueprint
+from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+  return 'Index'
+
+@main.route('/profile')
+def profile():
+  return 'Profile'

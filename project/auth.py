63ea633a79d5cfc66ed02972129623d21cd01f5b
aaron shivers <aaorn@moonmail.org>
2019-06-23 13:28:10 -0500

    added route for GET /signup

--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,5 @@
 # Import Dependencies
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -10,23 +10,28 @@ auth = Blueprint('auth', __name__)
 def login():
   return render_template('login.html')
 
-@auth.route('/signup', methods=['POST'])
+@auth.route('/signup', methods=['POST', 'GET'])
 def signup():
-  email = request.form.get('email')
-  name = request.form.get('name')
-  password = request.form.get('password')
+  if request.method == 'GET':
+    return render_template('signup.html')
 
-  user = User.query.filter_by(email=email).first()
+  else:
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
 
-  if user:
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+      flash('Email address already exists')
+      return redirect(url_for('auth.login'))
+
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
     return redirect(url_for('auth.login'))
-
-  new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-
-  db.session.add(new_user)
-  db.session.commit()
-
-  return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():

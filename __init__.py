4928c11b4690d63985741fcca42e25a8298314b0
aaron shivers <aaorn@moonmail.org>
2019-06-23 11:10:33 -0500

    setup __init__.py

---
+++ __init__.py
@@ -0,0 +1,32 @@
+# Import Dependencies
+from flask import Flask__
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+# Initialize Database
+db = SQLAlchemy()
+
+def create_app()
+
+  # Initialize Application
+  app = Flask(__name__)
+
+  # Configure Application
+  app.config['SECRET_KEY'] = 'this-is-a-secret'
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+  # Initialize Database
+  db.init_app(app)
+
+  # Blueprint for Auth Routes
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint)
+
+  # Blueprint for Non-Auth Routes
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+  return app
+
+
